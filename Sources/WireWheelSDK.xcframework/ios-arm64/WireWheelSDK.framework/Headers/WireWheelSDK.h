9de4ef58053a996186c5ea51b3f9323e2a3bb0d6
Wasim Raza <wasim.786raza@gmail.com>
2023-08-09 23:51:52 +0530

    Fixed Framework Error

---
+++ Sources/WireWheelSDK.xcframework/ios-arm64/WireWheelSDK.framework/Headers/WireWheelSDK.h
@@ -0,0 +1,18 @@
+//
+//  WireWheelSDK.h
+//  WireWheelSDK
+//
+//  Created by Simpalm iOS on 09/08/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for WireWheelSDK.
+FOUNDATION_EXPORT double WireWheelSDKVersionNumber;
+
+//! Project version string for WireWheelSDK.
+FOUNDATION_EXPORT const unsigned char WireWheelSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WireWheelSDK/PublicHeader.h>
+
+
