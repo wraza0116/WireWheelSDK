5b414ceb647c40e6497e189d410f3c7456d681ec
Wasim Raza <wasim.786raza@gmail.com>
2023-08-09 21:50:40 +0530

    Version Update

--- Package.swift
+++ Package.swift
@@ -19,7 +19,8 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .binaryTarget(name: "WireWheelSDK",
-                      path: "./Sources/WireWheelSDK.xcframework")
-        ]
+        .binaryTarget(
+            name: "WireWheelSDK",
+            path: "./Sources/WireWheelSDK.xcframework")
+    ]
 )
